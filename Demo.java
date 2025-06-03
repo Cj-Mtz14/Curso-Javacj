2a2a438bc95e30164a943aafe10fe8f0aa0ee7ce
Cristian Martínez <martj8561@gmail.com>
2025-06-03 12:55:42 +0200

    Add demo

---
+++ Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+    public static void main (String[] args) {
+
+        
+        String greeting = "Hola Mundo";
+        System.out.println("---------");
+        System.out.println(greeting);
+
+}
+
+}     
