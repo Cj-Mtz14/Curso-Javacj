ccf92eee043cadc28c47d138702cd1c76b0ebbd4
Cristian Martínez <martj8561@gmail.com>
2025-06-09 09:11:28 +0200

    car

---
+++ demos/Practic/Car.java
@@ -0,0 +1,14 @@
+
+
+public class Car {
+    public static void main(String[] args) {
+        
+        int asientos = 6;
+        String color = "azul";
+        int puertas = 4;
+        String marca = "Kia";
+        boolean isAuto = false;
+        char tipo = "A";
+    }
+    
+}
