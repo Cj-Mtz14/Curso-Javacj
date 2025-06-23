ed6fda7787badd13f86b78a2d77edc8da2f646a2
Cristian Martínez <martj8561@gmail.com>
2025-06-23 13:49:15 +0200

    dia 23 de junio

---
+++ demos-oop/demo01/src/accounting/Company.java
@@ -0,0 +1,20 @@
+package accounting;
+
+public class Company {
+     private String nif;
+    private String name;
+
+    public Company(String nif, String name) {
+        this.nif = nif;
+        this.name = name;
+    }
+
+    String getNif() {
+        return this.nif.toUpperCase();
+    }
+
+    String getName() {
+        return name;
+    }
+
+}
---
+++ demos-oop/demo01/src/base/Pet.java
@@ -0,0 +1,24 @@
+package base;
+
+public class Pet {
+
+    private String name;
+    private int age;
+
+    public Pet() {};
+
+    public Pet(String name, int age) {
+    this.name = name;
+    this.age = age;
+
+    };
+
+    public void sleep () {
+        System.out.println();
+    }
+
+    public void eat () {
+        System.out.println();
+    }
+
+}
