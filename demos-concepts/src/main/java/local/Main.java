d49292f36b34b00679fc032e745a7e9bd39009a4
Cristian Martínez <martj8561@gmail.com>
2025-06-30 13:47:25 +0200

    dia 30 de curso

---
+++ demos-concepts/src/main/java/local/Main.java
@@ -0,0 +1,19 @@
+package local;
+
+import local.concept1.Square;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+
+        Square sq = new Square(10);
+        
+
+
+
+
+    }
+}
